6eba743af53aab79a91968492357c680a832961b
Kholodov <kholodov.vadym@yandex.ru>
2024-08-28 13:42:19 +0300

    Разобьет код на модули

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { render } from './render';
+import FilterView from './view/filter-view';
 import NewPointButtonView from './view/new-point-button-view';
 
 const pageHeaderElement = document.querySelector('.page-header');
 const tripMainElement = pageHeaderElement.querySelector('.trip-main');
+const tripControlsFiltersElement = pageHeaderElement.querySelector(
+  '.trip-controls__filters'
+);
 
 render(new NewPointButtonView(), tripMainElement);
+render(new FilterView(), tripControlsFiltersElement);
