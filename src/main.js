696f73c5452bdaf0c8c5c8d086ba5ba96e60264a
Kholodov <kholodov.vadym@yandex.ru>
2024-08-28 14:23:57 +0300

    Добавит презентер доски

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import BoardPresenter from './presenter/board-presenter';
 import { render } from './render';
 import FilterView from './view/filter-view';
 import NewPointButtonView from './view/new-point-button-view';
@@ -8,5 +9,15 @@ const tripControlsFiltersElement = pageHeaderElement.querySelector(
   '.trip-controls__filters'
 );
 
+const pageMainElement = document.querySelector('.page-main');
+const pageBodyContainerElement = pageMainElement.querySelector(
+  '.page-body__container'
+);
+const boardPresenter = new BoardPresenter({
+  boardContainer: pageBodyContainerElement,
+});
+
 render(new NewPointButtonView(), tripMainElement);
 render(new FilterView(), tripControlsFiltersElement);
+
+boardPresenter.init();
--- src/view/board-view.js
+++ src/view/board-view.js
@@ -3,10 +3,6 @@ import { createElement } from '../render.js';
 const createBoardTemplate = () => `
   <section class="trip-events">
     <h2 class="visually-hidden">Trip events</h2>
-
-    <!-- Сортировка -->
-
-    <!-- Контент -->
   </section>
 `;
 
